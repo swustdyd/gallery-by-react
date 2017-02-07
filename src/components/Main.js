0d82732a94d676ff26e6a0a0744d9e43693a1e7a
aarondeng <1562044678@qq.com>
2017-02-07 13:03:06 +0800

    实现图片显示

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 var imageDatas = require('../data/imageDatas.json');
 
-imageDatas = (function (imageDatasArr) {
+imageDatas = ((imageDatasArr) => {
   for(var i = 0; i < imageDatasArr.length; i++){
     var singleImageData = imageDatasArr[i];
     singleImageData.imageUrl = '../images/' + imageDatasArr[i].fileName;
@@ -14,12 +14,38 @@ imageDatas = (function (imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 
+class ImgFigure extends React.Component{
+  render(){
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
 class GalleryByReactAppComponent extends React.Component {
   render() {
+    var imgFigures = [];
+    var controllerUnits = [];
+    for(var i = 0; i < imageDatas.length; i++){
+      imgFigures.push(<ImgFigure data={imageDatas[i]}/>);
+    }
+    /*imageDatas.forEach((value) => {
+      imgFigures.push(<ImgFigure data={value}/>);
+    }*/
+
     return (
       <section className="stage">
-        <section className="image-sec"></section>
-        <nav className="controller-nav"></nav>
+        <section className="image-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
       </section>
     );
   }
