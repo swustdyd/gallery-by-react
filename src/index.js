018c9b6758c3da3e41e0cd2396924649ce738c0f
aarondeng <1562044678@qq.com>
2017-02-15 15:09:04 +0800

    添加CommentBox组件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GalleryByReactApp from './components/Main';
+import CommentBox from './components/CommentBox';
+//import GalleryByReactApp from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<GalleryByReactApp />, document.getElementById('app'));
+ReactDOM.render(<CommentBox url="data/comments.json"/>, document.getElementById('app'));
